c05188d0045307b133891510e0e801c0b14e41d8
arnhaa <arnehaagensen@gmail.com>
2020-04-02 15:28:54 +0200

    added local variable for  account id and region

--- main.tf
+++ main.tf
@@ -3,6 +3,14 @@ provider "aws" {
   alias  = "certificate-provider"
 }
 
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+locals {
+  current_account_id = data.aws_caller_identity.current.account_id
+  current_region     = data.aws_region.current.name
+}
+
 data "aws_route53_zone" "main" {
   name = var.hosted_zone_name
 }
