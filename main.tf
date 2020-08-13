4e84df6ff3f4beaa5d45b1e88f5f96d8e69f66dd
arnhaa <arnehaagensen@gmail.com>
2020-08-13 13:06:04 +0200

    added roles example

--- main.tf
+++ main.tf
@@ -41,6 +41,16 @@ resource "aws_cognito_user_pool" "user_pool" {
       max_length = 2048 
 		}
     }
+	
+  schema {
+      attribute_data_type      = "String"
+      mutable                  = false
+      name                     = "roles"
+	  string_attribute_constraints {
+      min_length = 0
+      max_length = 2048 
+		}
+    }
   
 }
 
