df0565e137d772e00cfb94f811227f89db57d9eb
arnhaa <arnehaagensen@gmail.com>
2020-09-09 15:08:23 +0200

    Update main.py
    updated with code for adgroups

--- src/main.py
+++ src/main.py
@@ -1,27 +1,46 @@
 import json
 import boto3
-
+import os
 
 def lambda_handler(event, context):
     print(f'incoming event: {json.dumps(event)}')
+    grouprole='arn:aws:iam::231176028624:role/SAML-ReadOnlyRole'
+    rolegroup = []
+    adroles=''
 
     if (event['triggerSource'] != "TokenGeneration_HostedAuth"):
         return event
 
-    if 'custom:roles' not in event['request']['userAttributes']:
-        return event
+    if 'custom:groups' not in event['request']['userAttributes']:
+        adgroups = ''
+    else:
+        adgroups = event['request']['userAttributes']['custom:groups']
 
-    group = event['request']['userAttributes']['custom:roles']
-    client = boto3.client('cognito-idp')
-    groupinfo = client.get_group(
-        UserPoolId=event['userPoolId'],
-        GroupName=group
-    )
-    groupinfo = groupinfo.get("Group")
-    grouprole = groupinfo.get("RoleArn")
+    if 'custom:roles' not in event['request']['userAttributes']:
+        adroles =''
+    else:
+        rolegroup = event['request']['userAttributes']['custom:roles']
+        client = boto3.client('cognito-idp')
+        groupinfo = client.get_group(
+            UserPoolId=event['userPoolId'],
+            GroupName=rolegroup
+        )
+        groupinfo = groupinfo.get("Group")
+        grouprole = groupinfo.get("RoleArn")
+        
+    if (adgroups == adroles == ''):
+        return event
+        
+    adgroupsmapping = json.loads(os.environ["ADGROUPS"])
+    
+    for adgroup in adgroupsmapping:
+        adgroupid = adgroup.split(":")
+        if (adgroupid[0] in adgroups):
+            rolegroup.append(adgroupid[1])
 
     event["response"]["claimsOverrideDetails"] = {
-        "groupOverrideDetails": {"groupsToOverride": [group], "iamRolesToOverride": [grouprole],
+        "claimsToAddOrOverride": {"scope" : "adgroups"},
+        "groupOverrideDetails": {"groupsToOverride": rolegroup, "iamRolesToOverride": [grouprole],
                                  "preferredRole": grouprole}
     }
 
